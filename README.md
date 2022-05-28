b182a4418522ea31ded05cd4df6f8457609a8947
jus4fun150 <just4fun150@protonmail.com>
2022-05-28 16:27:36 -0400

    Finished with GUI

---
+++ README.md
@@ -0,0 +1,13 @@
+
+
+
+# **Rock Paper Scissors game**
+An update to the console game. 
+
+##### **What i learnt** 
+- More understanding of ```DOM manipulation``` and ```methods``` .
+- A bit more about using, adding and removing ```EventListeners```.
+- Code refactoring.
+
+[Live Preview](https://just4fun150.github.io/ROCK-PAPER-SCISSORS/) 👀
+
--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,34 @@
+
+const options = document.querySelectorAll('li');
+const result = document.querySelector('.result');
+const winner = document.querySelector('.winner');
+const pScore = document.querySelector('.pScore');
+const cScore = document.querySelector('.cScore');
+const subHeader = document.querySelector("h2");
+const restartBtn = document.querySelector('.restart');
+const optionDisplay = document.querySelector('.option-container');
+
+
+
 let playerScore = 0;
 let computerScore = 0;
-game();
-reportWinner();
+let playerPlay;
+
+
+
+restartBtn.style.display = 'none'; 
+
+//forEach method to iterate through each list
+options.forEach((option) => {
+    // add a click listener to each list and play game
+option.addEventListener('click', () => {
+        playerPlay = option.dataset.selection;
+        playRound(playerPlay,computerPlay());
+        reportWinner(); 
+    });
+});
+
+
 
 
 
@@ -9,56 +36,54 @@ function computerPlay() {
     let randomDecision;
     let options = ["scissors","paper","rock"]; 
     return randomDecision = options[Math.floor(Math.random() * options.length)];
-
 }
 
 
 function playRound(playerSelection, computerSelection) {
-        playerSelection = playerSelection.toLowerCase();
-    if ( (playerSelection ==="rock" && computerSelection ==="scissors") ||
-        (playerSelection ==="scissors" && computerSelection ==="paper") ||
-        (playerSelection ==="paper" && computerSelection ==="rock")){
-        ++playerScore;
-        console.log("you won the round!");
-    }
+    playerSelection = playerSelection.toLowerCase();
+if ( (playerSelection ==="rock" && computerSelection ==="scissors") ||
+    (playerSelection ==="scissors" && computerSelection ==="paper") ||
+    (playerSelection ==="paper" && computerSelection ==="rock")){
+    ++playerScore;
+    winner.textContent = `You won the round! ${playerSelection} beats ${computerSelection}`;
+    pScore.textContent = playerScore;
+}
 
-    else if ((playerSelection ==="scissors" && computerSelection ==="rock") ||
-            (playerSelection ==="paper" && computerSelection ==="scissors") ||
-            (playerSelection ==="rock" && computerSelection ==="paper"))  {
-            ++computerScore;
-        console.log("you lost the round");
-            
-    }
-   
-    else if (playerSelection === computerSelection ) { 
-        return console.log("it's a draw");
-    }
+else if ((playerSelection ==="scissors" && computerSelection ==="rock") ||
+        (playerSelection ==="paper" && computerSelection ==="scissors") ||
+        (playerSelection ==="rock" && computerSelection ==="paper"))  {
+        ++computerScore;
+        winner.textContent = `You lost the round! ${computerSelection} beats ${playerSelection}`;
+        cScore.textContent = computerScore;       
+}
 
-    else if(playerSelection === null || playerSelection === undefined || " "){
-            ++computerScore;
-        console.log("you lost the round" );
+else {
+    //else if playerSelection === computerSelection  
+    winner.textContent = "It's a tie!";
     }
 }  
 
 
-function game () {
-    for( let i = 0; i < 5; i++) {
-        const playerPlay = prompt("What's your pick");
-        console.log(playRound(playerPlay,computerPlay()));
-    }
-}
-
-
-
 function reportWinner() {
 
-    if(playerScore > computerScore) {
-        console.log('You are the winner! Scores is' + " " + playerScore + ":" + computerScore);
+    if(playerScore === 5) {
+        result.textContent = "Final result";
+        winner.textContent = "Congrats! You won the game.";
+        subHeader.style.display = 'none';
+        optionDisplay.remove();
+        restartBtn.style.display = 'inherit';
+        restartBtn.addEventListener('click',() => location.reload());
     }
-    else if (playerScore === computerScore) {
-        console.log('No winner! Scores is' + " " + computerScore + ":" + playerScore );
-    }
-    else{
-    console.log('Computer is the winner! Scores is' + " " + computerScore + ":" + playerScore);
-    }
+    else if (computerScore === 5) {
+        result.textContent = "Final result";
+        winner.textContent = "Oh no! Computer won the game.";
+        subHeader.style.display = 'none';
+        optionDisplay.remove();
+        restartBtn.style.display = 'inherit';
+        restartBtn.addEventListener('click',() => location.reload());
+    }   
+    
 }
+
+
+
